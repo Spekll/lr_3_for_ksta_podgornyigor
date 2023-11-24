60ae0d6cdfad5e02309b3a4a8d180c6cbcbaff9d
podgornyigor <igor.podgorny@inbox.ru>
2023-11-24 15:31:48 +0300

    rebuild only for third values

--- Fibo/main.cpp
+++ Fibo/main.cpp
@@ -6,23 +6,24 @@ int main()
 {
     int N;
     int counter = 1;
-    cout << "Vvedite kol-vo iteraciy N: ";
+    cout << "Vvedite kol-vo iteraciy N:";
     cin >> N;
-
     int a = 0, b = 1, nextTerm;
-    cout << "Ryad chisel Fibonacci do " << N << " iteraciy: ";
+    cout <<"Ryad chisel Fobonacci do " << N << "iteraciy: ";
 
-    for (int i = 1; i <= N; ++i)
-        {
+
+    for (int i = 1; i <= N; i++) {
+
         if (counter == 3)
             {
+
             cout << a << " ";
             counter = 0;
-            }
+        }
         nextTerm = a + b;
         a = b;
         b = nextTerm;
         counter++;
-        }
+    }
     return 0;
 }
